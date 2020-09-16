a4a37e4ecdc109dcc59cf66fa10ef7cd3525e8f5
FedericoAvano <federco.m.avano@gmail.com>
2020-09-16 10:16:34 +0200

    Add existing file

---
+++ prova-button-trekk/src/components/button.js
@@ -0,0 +1,10 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { css } from "glamor"
+
+
+<script src = "https://bw.trekksoft.com/loader.js"
+async data-owner = "trekksoft" data-type = "activity-booking"
+data-id = "299727"
+data-client-id = "BOOKIE-20dda9f1abd2c734211589943"
+data-token = "bc475b3c4053e86af992e8f40cbf4c514f33f3c5a7634c50"> </script>
